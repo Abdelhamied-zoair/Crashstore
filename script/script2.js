38aa63c8f06de7d8323fd36d15bd5f3ab1a5c5b7
Abdelhamied-zoair <abdulhamiedzoair@gmail.com>
2022-09-06 18:13:01 -0700

    init

---
+++ script/script2.js
@@ -0,0 +1,10 @@
+// function validate(){
+//     var email=document.getElementById("email").value;
+//     var password=document.getElementById("password").value;
+//     var message=document.getElementById("message");
+// }
+// if(email.indexof("@")==-1){
+//     text="please enter your email"
+//     errorMeesage.innerHTML=text;
+
+// }
